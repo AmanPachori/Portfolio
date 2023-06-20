d99d80ae803763d901f8897d551697d598b85044
AmanPachori <amangirrajpachori2002@gmail.com>
2023-06-20 23:14:08 +0530

    minor fixes

--- src/Constants.js
+++ src/Constants.js
@@ -3,14 +3,12 @@ export const LINKEDIN_URL =
 export const GITHUB_URL = "https://github.com/AmanPachori/";
 export const RESUME_URL =
   "https://drive.google.com/file/d/1bRPcTwBYkFIUCZxyheS_bfmug1zJRoUh/view";
-export const INSTAGRAM_URL = "https://www.instagram.com/kawaljeet_sb/";
+export const INSTAGRAM_URL = "";
 export const EMAIL_URL = "mailto:amangirrajpachori2002@gmail.com";
-export const ARTSTATIONURL = "https://www.artstation.com/kawal2001singh";
+export const ARTSTATIONURL = "";
 export const LOCATION_URL = "https://goo.gl/maps/";
 export const PROFILE_PHOTO_URL =
   "https://avatars.githubusercontent.com/u/84467090?v=4";
 export const LEETCODE_URL = "https://leetcode.com/batrakawaljeetsingh/";
-export const GEEKSFORGEEKS_URL =
-  "https://auth.geeksforgeeks.org/user/kawaljeet/profile";
-export const HACKERRANK_URL =
-  "https://www.hackerrank.com/batrakawaljeets1?hr_r=1";
+export const GEEKSFORGEEKS_URL = "";
+export const HACKERRANK_URL = "";
